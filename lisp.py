b11c139541a7ffcdffb5863ebd930249bdab88d8
unknown <RK@.(none)>
2010-12-22 17:49:18 +0000

    Added some basic maths functions. Added support for comments. Moved REPL and main method to here.

--- lisp.py
+++ lisp.py
@@ -1,4 +1,9 @@
-#! /usr/bin/python
+#
+# PyLisp / Pyle (Python List Expressions)
+#
+# Ronan Klyne, http://github.com/users/rklyne
+#
+
 # imports
 import sys
 import re
@@ -42,12 +47,39 @@ def lisp_apply(f, args, env):
     finally:
         assert env_stack.pop() is env
 
-def plus(*args):
-    return sum(args)
+def build_basic_env():
 
-basic_env = LispEnv(**{
-    '+': plus,
-})
+# Maths functions
+    def plus(*args):
+        return sum(args)
+    def times(*args):
+        result = 1
+        for arg in args:
+            result *= arg
+        return result
+    def minus(l, r):
+        return l - r
+    def divide(l, r):
+        return l / r
+
+# IO functions
+    def print_func(*args):
+        for arg in args:
+            print arg,
+        print
+
+# The environment, with Lisp names.
+    env = LispEnv(**{
+        '+': plus,
+        '-': minus,
+        '*': times,
+        '/': divide,
+        'print': print_func,
+    })
+
+    return env
+
+basic_env = build_basic_env()
 
 def resolve_def(var, env):
     if not env.has_key(var):
@@ -59,28 +91,38 @@ def resolve_def(var, env):
 
 class Reader(object):
     _prompt = 'Lisp --> '
+    _nl_expr = re.compile(r"[\\\n\\\r]")
     _ws_expr = re.compile(r"[\\\t\\\n\\\r, ]")
     _ws_char = ' '
+
     def __init__(self, input_func=raw_input):
         self._raw_input = input_func
         self._buffer = ''
-    def _input(self, prompt):
+
+    def _input(self):
         data = ''
         while not data:
-            data = self._raw_input(prompt)
+            data = self._raw_input(self._prompt)
         return data + "\n" # This nl gets stripped by python :-(
     def _read(self):
-        data = str(self._input(self._prompt))
+        data = str(self._input())
         # Replace all whitespace chars with the one true ws-char.
-        data = self._ws_expr.sub(self._ws_char, data)
+#        data = self._ws_expr.sub(self._ws_char, data)
         self._buffer += data
-    def _drop_ws(self):
-        while self._ws_expr.match(self.peek_char()):
+
+    def _drop_until(self, expr, matches=True):
+        while (expr.match(self.peek_char()) is not None) == matches:
             self._buffer = self._buffer[1:]
+    def _drop_ws(self):
+        self._drop_until(self._ws_expr)
+    def _drop_until_newline(self):
+        self._drop_until(self._nl_expr, False)
+
     def get_char(self):
         c = self.peek_char()
         c, self._buffer = self._buffer[0], self._buffer[1:]
         return c
+
     def peek_char(self):
         if not self._buffer:
             self._read()
@@ -92,10 +134,13 @@ class Reader(object):
 
     def get_expr(self,
         digits='0123456789',
-        symbol_start_chars='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_+-!?$^~#',
+        symbol_start_chars='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_+-/*!?$^~#',
         symbol_chars=''
     ):
-        c = self.get_non_ws_char()
+        c = self.get_non_ws_char() # First char.
+        if c == ';':
+            self._drop_until_newline()
+            return None
         if c == '(':
             self._drop_ws()
             s_expr = []
@@ -122,12 +167,14 @@ class Reader(object):
         raise ReaderError("Unexpected symbol", c, self._buffer)
 
 class FileReader(Reader):
+    _CHUNK = 256
     def __init__(self, filename):
         self._fp = open(filename, 'rb')
-        def input_func(prompt):
-            return self._fp.readline()
-        super(FileReader, self).__init__(input_func=input_func)
-
+        super(FileReader, self).__init__()
+    def _input(self):
+        data = self._fp.read(self._CHUNK)
+        if data: return data
+        raise EOFError
     def __iter__(self):
         try:
             while True:
@@ -179,3 +226,81 @@ def lisp_eval(expr, env=basic_env):
 run = lisp_eval
 
 
+def repl(debug=False):
+    """REPL:
+    * R ead s-expressions and
+    * E valuate them, then
+    * P rint the results.
+    * L oop."""
+    reader = Reader()
+    try:
+        while 1:
+            try:
+                expr = reader.get_expr()
+                if debug: print "READ:", expr
+            except LispError:
+                print "READER ERROR:"
+                import traceback
+                traceback.print_exc()
+                continue
+            try:
+                s = run(expr)
+            except LispError:
+                print "EVAL ERROR:"
+                import traceback
+                traceback.print_exc()
+            except KeyboardInterrupt: break
+            else:
+                try:
+                    print s
+                except:
+                    print '???'
+    except (EOFError, KeyboardInterrupt):
+        print "Done"
+
+
+def usage():
+    print """Usage:
+    lisp.py            # Runs a Read-Eval-Print Loop.
+    lisp.py file.pyl   # Executes file.pyl
+    """
+
+
+def main():
+    import sys, getopt
+
+    args_in = sys.argv[1:]
+    longopts = [
+        'help',
+        'verbose',
+    ]
+    shortopts = '?hv'
+    opts, args = getopt.getopt(args_in, shortopts, longopts)
+
+    opt_help = False
+    opt_verbose = 0
+
+    for opt, value in opts:
+        if opt in ['-h', '-?', '--help']:
+            opt_help = True
+        elif opt in ['-v', '--verbose']:
+            opt_verbose += 1
+
+    debug = (opt_verbose >= 1)
+
+    if opt_help:
+        usage()
+        return
+    elif not args:
+        repl(debug=debug)
+    else:
+        # Each spare arg is interpreted as a file name
+        for filename in args:
+            if debug:
+                print "Reading", filename
+            reader = FileReader(filename)
+            for expr in reader:
+                result = lisp_eval(expr)
+
+if __name__ == '__main__':
+    main()
